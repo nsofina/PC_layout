7d424f32149388bb2022d8a5ea5e46b9c012839f
nsofina <sonata-24@yandex.ru>
2022-04-24 13:40:13 +0300

    add timer

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,4 +1,5 @@
-const contents = document.querySelectorAll('.program-line__content')
+//Первый вариант
+/*const contents = document.querySelectorAll('.program-line__content')
 
 contents.forEach((elem) => {
     const title = elem.querySelector('.program-line__title');
@@ -13,5 +14,29 @@ contents.forEach((elem) => {
                 descr.classList.remove('active');
             }
         }) 
+    })
+})*/
+
+
+//Второй вариант
+const contents = document.querySelectorAll('.program-line__content')
+const contentsDescr = document.querySelectorAll('.program-line__descr')
+
+contents.forEach((elem) => {
+    const title = elem.querySelector('.program-line__title')
+    const descr = elem.querySelector('.program-line__descr')
+
+    descr.style.transition = 'height .3s'
+    descr.style.overflow = 'hidden'
+
+    title.addEventListener('click', () => {
+        contentsDescr.forEach((tab) => {
+            if (tab === descr) {
+                tab.style.height = tab.scrollHeight + 'px'
+            }
+            else {
+                tab.style.height = 0
+            }
+        })
     })
 })
